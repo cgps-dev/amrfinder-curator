2b77b51b01e10007a987d84fdad69af2ceb21402
Khalil <khalil.abudahab@cgps.group>
2024-02-26 12:53:36 +0000

    Create curated-mechanisms.js

---
+++ parser/curated-mechanisms.js
@@ -0,0 +1,20 @@
+[
+  // 1280
+  [ "1280", "METHICILLIN", "mecA", [ "mecA" ] ],
+  [ "1280", "METHICILLIN", "mecC", [ "mecC" ] ],
+  [ "1280", "METHICILLIN", "mecC2", [ "mecC2" ] ],
+
+  [ "1280", "VANCOMYCIN", "vanA", [ "vanA" ] ],
+
+  [ "1280", "VANCOMYCIN", "vanH-A", [ "vanA", "vanH-A" ] ],
+  [ "1280", "VANCOMYCIN", "vanR-A", [ "vanA", "vanR-A" ] ],
+  [ "1280", "VANCOMYCIN", "vanS-A", [ "vanA", "vanS-A" ] ],
+  [ "1280", "VANCOMYCIN", "vanX-A", [ "vanA", "vanX-A" ] ],
+  [ "1280", "VANCOMYCIN", "vanY-A", [ "vanA", "vanY-A" ] ],
+  [ "1280", "VANCOMYCIN", "vanZ-A", [ "vanA", "vanZ-A" ] ],
+
+  //470
+
+  // 287
+
+]
