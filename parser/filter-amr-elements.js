16b6825a653c5e94c2d079f86528695dda79e554
Khalil <khalil.abudahab@cgps.group>
2024-02-27 18:12:52 +0000

    Update filter-amr-elements.js

--- parser/filter-amr-elements.js
+++ parser/filter-amr-elements.js
@@ -1,8 +1,16 @@
+const bannedMethods = [
+  "PARTIALX",
+  "PARTIALP",
+  "PARTIAL_CONTIG_ENDX",
+  "PARTIAL_CONTIG_ENDP",
+  "INTERNAL_STOP",
+];
+
 function filterAmrElements(list) {
   const filtered = [];
 
   for (const item of list) {
-    if (item["element_type"] === "AMR") {
+    if (item["element_type"] === "AMR" && !bannedMethods.includes(item["method"])) {
       filtered.push(item);
     }
   }
